379bb663b8319d355658dc5a0818c0aaf82e6dd9
Bogdan <bogdanscherbinadev@gmail.com>
2024-01-22 16:43:00 +0200

    feat: implement cd command

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ import logWorkingDirPath from './views/logWorkingDirPath.js';
 import readline from 'readline';
 import * as os from 'os';
 import { WAITING } from './const.js';
-import { up } from './controllers/controller.js';
+import { cd, up } from './controllers/controller.js';
 
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 process.chdir(os.homedir());
 process.on('beforeExit', exit);
-rl.on('line', async (l) => {
-  if (l === 'up') {
-    up();
+rl.on('line', async (line) => {
+  switch (line) {
+    case 'up':
+      up();
+      break;
+    case line.startsWith('cd'):
+      const specifiedPath = line.split(' ').at(1);
+      cd(specifiedPath);
+      break;
   }
 
   logWorkingDirPath();
--- controllers/controller.js
+++ controllers/controller.js
@@ -4,3 +4,8 @@ export const up = () => {
   const dirUp = join(process.cwd(), '..');
   process.chdir(dirUp);
 };
+
+export const cd = (path) => {
+  const newDir = join(process.cwd(), path);
+  process.chdir(newDir);
+};
