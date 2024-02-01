131820a5a9a0188083827cea8b86ba79bbbae3cc
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-01 17:44:59 +0200

    feat: add osController JSDoc comment

--- controllers/osController.js
+++ controllers/osController.js
@@ -1,9 +1,17 @@
 import * as os from 'os';
 
+/**
+ * Prints the end-of-line marker for the current operating system.
+ * @returns {void} Nothing.
+ */
 export const eol = () => {
   console.log(JSON.stringify(os.EOL));
 };
 
+/**
+ * Prints the information about the CPUs of the current machine.
+ * @returns {void} Nothing.
+ */
 export const cpus = () => {
   const cpuData = os.cpus();
 
@@ -16,20 +24,32 @@ export const cpus = () => {
     };
   });
 
-  const half = Math.floor(tableData.length / 2);
+  const half = Math.floor(tableData.length / 2); // TODO: handle different systems
   tableData.at(half)['Amount of CPUS'] = cpuData.length;
 
   console.table(tableData);
 };
 
+/**
+ * Prints the home directory of the current user.
+ * @returns {void} Nothing.
+ */
 export const homedir = () => {
   console.log(os.homedir());
 };
 
+/**
+ * Prints the username of the current user.
+ * @returns {void} Nothing.
+ */
 export const username = () => {
   console.log(os.userInfo().username);
 };
 
+/**
+ * Prints the architecture of the current operating system.
+ * @returns {void} Nothing.
+ */
 export const architecture = () => {
   console.log(os.arch());
 };
