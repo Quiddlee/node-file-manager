174bde3d2fb15cba9eb57a4c73db2bebdcd6f453
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-01 17:13:28 +0200

    feat: improve comand line parsing

--- views/logWait.js
+++ views/logWait.js
@@ -1,5 +1,8 @@
+import paint from '../lib/helpers/paint.js';
+import { WAITING } from '../lib/const/const.js';
+
 const logWait = () => {
-    console.log(paint(WAITING, 'cyan', 'italic'));
-}
+  console.log(paint(WAITING, 'cyan', 'italic'));
+};
 
 export default logWait;
